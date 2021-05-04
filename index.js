f0b0079dec9803939a8f2e7fe134ba8f8bbb5fd7
KritikaSharmaKS <sh.kritika01@gmail.com>
2021-05-03 21:41:23 -0400

    insert/select second customer

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const Customer = require("./models/customer");
 const Order = require("./models/order");
 
 let customerId = null;
-new Customer({name: "Chandler Bing", email: "cb@gmail.com"})
+Customer
+    .forge({name: "Monica Geller", email: "mg@gmail.com"})
     .save()
     .then(customer => {
-        console.log("First Customer Created: ", customer);
-        new Customer({id: customer.id})
+        console.log("Second Customer Created: ", customer);
+        return Customer
+            .forge({id: customer.id})
             .fetch()
-            .then((model) => {
-                console.log(model.get('id'));
-            });
+    })
+    .then((model) => {
+        console.log(model.get('id'));
     })
     .catch((err) => {
         console.log(err);
